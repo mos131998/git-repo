c3f2b32acb452c3a445577dc54c7dcd2a84711fe
mos131998 <mos131998@gmail.com>
2026-03-23 14:57:47 +0700

    Squashed commit of the following:
    commit 16ae283db3a751a57d9e1cc8ffb74303bf671016
    Author: mos131998 <mos131998@gmail.com>
    Date:   Mon Mar 23 14:12:59 2026 +0700
    
        feat(workspace): generate workspace resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { WorkspaceModule } from './workspace/workspace.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, AuthModule, WorkspaceModule],
 
   controllers: [AppController],
   providers: [AppService],
