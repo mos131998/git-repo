7dc7a28757eb5335d99850c781d0ffdadce8ae60
mos131998 <mos131998@gmail.com>
2026-03-23 15:37:56 +0700

    feat(boards): generate boards resource (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [AuthModule, UserModule, AuthModule, WorkspaceModule],
+  imports: [AuthModule, UserModule, AuthModule, WorkspaceModule, BoardsModule],
 
   controllers: [AppController],
   providers: [AppService],
